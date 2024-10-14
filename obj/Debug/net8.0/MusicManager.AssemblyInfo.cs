af909d5c1ebae35fa36280d7be5c9ef733a4f784
Etienne LANTERNIER <elanternier@consultants-solutec.fr>
2024-10-14 10:35:14 +0200

    ca marche pas

--- obj/Debug/net8.0/MusicManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95db83ec0c2e0d44855594302fe21fd4d7ac6bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80183d82195dee1a2b5fa61cbb1555411775923")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
